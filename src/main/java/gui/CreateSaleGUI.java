051357433a89623f91d1fd7a68e23ef8f1534b9a
iago <iago@iago-IdeaPad-Slim-3-15IRH10>
2026-09-24 13:01:05 +0200

    Merge branch 'master' of https://github.com/Pablo-Martinez-Anorga/Market-Lana

--- src/main/java/gui/CreateSaleGUI.java
+++ src/main/java/gui/CreateSaleGUI.java
@@ -122,8 +122,11 @@ public class CreateSaleGUI extends JFrame {
 		jLabelError.setForeground(Color.red);
 		
 	    status=Utils.getStatus();
-		if(status!=null) {for(String s:status) statusOptions.addElement(s);}
-
+		if(status==null) {
+			System.out.println("erabilida en, es edo eus");
+		}else {
+			for(String s:status) statusOptions.addElement(s);
+		}
 		this.getContentPane().add(jLabelMsg, null);
 		this.getContentPane().add(jLabelError, null);
 
